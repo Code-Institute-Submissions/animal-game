e97396b52c2c77f99fc8b4ad086226970ceba1df
LawlessXD <DavidLawless@gmail.com>
2020-03-07 11:57:40 +0000

    Created object array for cards

---
+++ assets/js/script.js
@@ -0,0 +1,56 @@
+const cards = [
+    {
+        "family": "cow",
+        "type": "parent",
+        "src": "assets/images/cow.png"  
+    },
+    {   "family": "cow",
+        "type": "child",
+        "src": "assets/images/calf.png" 
+    },
+    {
+        "family": "dog",
+        "type": "parent",
+        "src": "assets/images/dog.jpg"  
+    },
+    {   "family": "dog",
+        "type": "child",
+        "src": "assets/images/puppy.jpg" 
+    },
+    {
+        "family": "sheep",
+        "type": "parent",
+        "src": "assets/images/sheep.jpg"  
+    },
+    {   "family": "sheep",
+        "type": "child",
+        "src": "assets/images/lamb.jpg" 
+    },
+    {
+        "family": "cat",
+        "type": "parent",
+        "src": "assets/images/cat.jpg"  
+    },
+    {   "family": "cat",
+        "type": "child",
+        "src": "assets/images/kitten.jpg" 
+    },
+    {
+        "family": "horse",
+        "type": "parent",
+        "src": "assets/images/horse.jpg"  
+    },
+    {   "family": "horse",
+        "type": "child",
+        "src": "assets/images/colt.jpg" 
+    },
+    {
+        "family": "lion",
+        "type": "parent",
+        "src": "assets/images/lion.jpg"  
+    },
+    {   "family": "lion",
+        "type": "child",
+        "src": "assets/images/cub.jpg" 
+    }
+];
