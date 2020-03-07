bea02117d062b698c4f682f7047fe9e4a3aad414
LawlessXD <DavidLawless@gmail.com>
2020-03-07 15:39:31 +0000

    Updated CSS for header section

--- assets/js/script.js
+++ assets/js/script.js
@@ -36,15 +36,19 @@ function fisherYatesShuffle(array) {
   return array;
 }
 
-// self executing function to create the deck 
-(function createDeck() {
-    for(x = 0; x < cards.length; x++) {
-        var src = cards[x].src;
-        console.log(typeof src);
-        console.log("Testing " +`${src}`);
-        card = document.createElement("div");
-        card.className = "card";
-        card.innerHTML = `<div class="card-inner"><div class="card-front"><img src="assets/images/card-front.jpg" alt="card front face"></div><div class="card-back"><img src="${src}" alt="card back face"></div></div>`;
-        deck.appendChild(card);
-    } 
- })();
+// self executing function to create the deck
+function createDeck() {
+    deck.innerHTML = "";
+  for (x = 0; x < cards.length; x++) {
+    var src = cards[x].src;
+    console.log(typeof src);
+    console.log("Testing " + `${src}`);
+    card = document.createElement("div");
+    card.className = "card";
+    card.innerHTML = `<div class="card-inner"><div class="card-front"><img src="assets/images/card-front.jpg" alt="card front face"></div><div class="card-back"><img src="${src}" alt="card back face"></div></div>`;
+    deck.appendChild(card);
+  }
+};
+
+createDeck();
+
