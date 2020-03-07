ac97b9d07f70d694991a249d2a0ebb027d4863c5
LawlessXD <DavidLawless@gmail.com>
2020-03-07 12:07:46 +0000

    Added fisherYatesShuffle function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,56 +1,34 @@
 const cards = [
-    {
-        "family": "cow",
-        "type": "parent",
-        "src": "assets/images/cow.png"  
-    },
-    {   "family": "cow",
-        "type": "child",
-        "src": "assets/images/calf.png" 
-    },
-    {
-        "family": "dog",
-        "type": "parent",
-        "src": "assets/images/dog.jpg"  
-    },
-    {   "family": "dog",
-        "type": "child",
-        "src": "assets/images/puppy.jpg" 
-    },
-    {
-        "family": "sheep",
-        "type": "parent",
-        "src": "assets/images/sheep.jpg"  
-    },
-    {   "family": "sheep",
-        "type": "child",
-        "src": "assets/images/lamb.jpg" 
-    },
-    {
-        "family": "cat",
-        "type": "parent",
-        "src": "assets/images/cat.jpg"  
-    },
-    {   "family": "cat",
-        "type": "child",
-        "src": "assets/images/kitten.jpg" 
-    },
-    {
-        "family": "horse",
-        "type": "parent",
-        "src": "assets/images/horse.jpg"  
-    },
-    {   "family": "horse",
-        "type": "child",
-        "src": "assets/images/colt.jpg" 
-    },
-    {
-        "family": "lion",
-        "type": "parent",
-        "src": "assets/images/lion.jpg"  
-    },
-    {   "family": "lion",
-        "type": "child",
-        "src": "assets/images/cub.jpg" 
-    }
+  { family: "cow", type: "parent", src: "assets/images/cow.png" },
+  { family: "cow", type: "child", src: "assets/images/calf.png" },
+  { family: "dog", type: "parent", src: "assets/images/dog.jpg" },
+  { family: "dog", type: "child", src: "assets/images/puppy.jpg" },
+  { family: "sheep", type: "parent", src: "assets/images/sheep.jpg" },
+  { family: "sheep", type: "child", src: "assets/images/lamb.jpg" },
+  { family: "cat", type: "parent", src: "assets/images/cat.jpg" },
+  { family: "cat", type: "child", src: "assets/images/kitten.jpg" },
+  { family: "horse", type: "parent", src: "assets/images/horse.jpg" },
+  { family: "horse", type: "child", src: "assets/images/colt.jpg" },
+  { family: "lion", type: "parent", src: "assets/images/lion.jpg" },
+  { family: "lion", type: "child", src: "assets/images/cub.jpg" }
 ];
+
+function fisherYatesShuffle(array) {
+  var currentIndex = array.length,
+    temporaryValue,
+    randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
